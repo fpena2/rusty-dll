cfaee7d68e3d264a69ea5745143d74145ddaaf53
fpena2 <f.j.pena@wustl.edu>
2023-11-27 00:29:20 -0600

    Add a shared library to handle a callback

--- include/example.h
+++ include/example.h
@@ -4,25 +4,30 @@
  * This is a dummy class to demonstrate features of the boiler plate.
  */
 class Dummy {
-	public:
-
+public:
   /**
    * Default constructor for Dummy (does nothing).
    */
   Dummy();
+
   /**
    * Returns a bool.
    * @return Always True.
    */
   bool doSomething();
-};
 
+private:
+  // Handle for the loaded library
+  void *libraryHandle;
+  typedef int (*AddFunction)(int, int);
+  // Function pointer for the add() function in the library
+  AddFunction addFunction;
+};
 
 #ifdef ENABLE_DOCTEST_IN_LIBRARY
 #include "doctest/doctest.h"
-TEST_CASE("we can have tests in headers if we want")
-{
-    Dummy d;
-    CHECK(d.doSomething() == true);
+TEST_CASE("we can have tests in headers if we want") {
+  Dummy d;
+  CHECK(d.doSomething() == true);
 }
 #endif
--- src/example.cpp
+++ src/example.cpp
@@ -1,21 +1,39 @@
 #include "example.h"
 
-Dummy::Dummy() {
+#include <dlfcn.h>
+#include <iostream>
 
+Dummy::Dummy() : libraryHandle(nullptr), addFunction(nullptr) {
+  libraryHandle = dlopen("../target/release/librusty_dll.so", RTLD_LAZY);
+  if (libraryHandle != nullptr) {
+    addFunction = reinterpret_cast<AddFunction>(dlsym(libraryHandle, "add"));
+  } else {
+    std::cerr << "Failed to load library: " << dlerror() << std::endl;
+  }
 }
 
 bool Dummy::doSomething() {
-    // Do silly things, using some C++17 features to enforce C++17 builds only.
-    constexpr int digits[2] = {0, 1};
-    auto [zero, one] = digits;
-    return zero + one;
-}
+  // Do silly things, using some C++17 features to enforce C++17 builds only.
+  constexpr int digits[2] = {100, 101};
+  auto [firstDigit, secondDigit] = digits;
 
+  std::cout << "C++ wants to add: " << firstDigit << " and " << secondDigit
+            << std::endl;
+
+  if (addFunction != nullptr) {
+    // Call the add() function from the library
+    int result = addFunction(firstDigit, secondDigit);
+    std::cout << "Result from Rust's add() function: " << result << std::endl;
+    return true;
+  }
+
+  std::cerr << "Failed to get function from library" << std::endl;
+  return false;
+}
 
 #ifdef ENABLE_DOCTEST_IN_LIBRARY
 #include "doctest/doctest.h"
-TEST_CASE("we can have tests written here, to test impl. details")
-{
-    CHECK(true);
+TEST_CASE("we can have tests written here, to test impl. details") {
+  CHECK(true);
 }
 #endif
